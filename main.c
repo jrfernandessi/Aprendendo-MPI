eea77fca901352ca3f13938133612ae28cf3c918
Junior Fernandes <jrfernandessi@users.noreply.github.com>
2020-01-05 17:50:44 -0300

    remove comments

--- main.c
+++ main.c
@@ -12,17 +12,14 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank == 0) {
-//        printf("enviando\n");
         strcpy(message, "Hello, World");
         for (i = 0; i < size; i++)
             MPI_Send(message, 13, MPI_CHAR, i, type, MPI_COMM_WORLD);
         printf("enviado");
     }else{
-//        printf("recebendo\n");
         MPI_Recv(message, 13, MPI_CHAR, 0, type, MPI_COMM_WORLD, &status);
         printf("recebido");
     }
-//    printf("imprimir mensagem\n");
     printf("Message from node=%d: %.13s\n ", rank, message);
     MPI_Finalize();
     return 0;
