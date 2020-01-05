63e56327be03b58182dbf33d10ca002649b67ad6
Junior Fernandes <jrfernandessi@users.noreply.github.com>
2020-01-05 14:25:56 -0300

    code before change of section 6.6

--- cmake-build-debug/CMakeFiles/Escalonador.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Escalonador.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/junior/git/Escalonador/main.c" "/home/junior/git/Escalonador/cmake-build-debug/CMakeFiles/Escalonador.dir/main.c.o"
+  "/Users/Junior/git/Learning-MPI/main.c" "/Users/Junior/git/Learning-MPI/cmake-build-debug/CMakeFiles/Escalonador.dir/main.c.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include "mpi.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
-int main(argc, argv)
-    int argc;
-    char **argv;
+int main(int argc, char **argv)
 {
+    //rank is the proccess id and nprocs is numbers of procces
     int rank, nprocs;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//    printf("%d\n", nprocs);
+//    printf("%d\n", rank);
     printf("Hello, World!\n");
     MPI_Finalize();
     return 0;
