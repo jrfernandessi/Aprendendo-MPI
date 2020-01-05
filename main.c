3f6be5f21129b61e41232bcef318dc380b3788dd
Junior Fernandes <jrfernandessi@users.noreply.github.com>
2020-01-05 15:08:46 -0300

    trying run example section 6.7

--- main.c
+++ main.c
@@ -1,18 +1,29 @@
-#include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include "mpi.h"
 
 int main(int argc, char **argv)
 {
-    //rank is the proccess id and nprocs is numbers of procces
-    int rank, nprocs;
+    char message[20];
+    int i, rank, size, type=99;
+    MPI_Status status;
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//    printf("%d\n", nprocs);
-//    printf("%d\n", rank);
-    printf("Hello, World!\n");
+    if (rank == 0) {
+        printf("enviando\n");
+        strcpy(message, "Hello, World");
+        for (i = 0; i < size; i++)
+            MPI_Send(message, 13, MPI_CHAR, i, type, MPI_COMM_WORLD);
+        printf("enviado");
+    }else{
+        printf("recebendo\n");
+        MPI_Recv(message, 13, MPI_CHAR, 0, type, MPI_COMM_WORLD, &status);
+        printf("recebido");
+    }
+//    printf("imprimir mensagem\n");
+    printf("Message from node=%d: %.13s\n ", rank, message);
     MPI_Finalize();
     return 0;
 }
